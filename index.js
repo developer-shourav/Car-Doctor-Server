385975e06d31acedde0883f8d603eea17f9d4461
developer-shourav <mrinmoyraj823@gmail.com>
2023-05-16 06:25:51 +0600

    primary server template added

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const port = process.env.PORT || 9000 ;
+
+
+
+// Middleware 
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.send('Welcome to Car Doctor Server')
+})
+
+
+
+
+app.listen(port, () => {
+    console.log(`Our Car Doctor Server is Running on the PORT: ${port}`);
+})
